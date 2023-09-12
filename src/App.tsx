e2e162245b04ba561de889fa220a2b3fb6f9b688
kyoungmin <lanos5019@ajou.ac.kr>
2023-09-12 13:39:35 +0900

    header component complete

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Main from "./layout/main";
+
+function App() {
+  return <Main />;
+}
+
+export default App;
---
+++ src/layout/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "../component/header/header";
+
+function Main() {
+  return <Header />;
+}
+
+export default Main;
