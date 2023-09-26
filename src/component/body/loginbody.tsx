299df2671e3c8c671fa7df69393518f154e7741a
kmkkkp <kmkkkp@ajou.ac.kr>
2023-09-26 23:02:20 +0900

    normalLogin

--- src/component/body/loginbody.tsx
+++ src/component/body/loginbody.tsx
@@ -1,20 +1,37 @@
 import { Link, Route, Routes } from "react-router-dom";
 
+
 import NaverLogin_Button from "../login/naverLogin";
 import KakaoLogin_Button from "../login/kakaoLogin";
 import GoogleLogin_Button from "../login/googleLogin";
+import NormalLogin from "../login/normalLogin";
 
 function Login_Body() {
+
   return (
-    <div>
-      <Link to="/naver">
-        <NaverLogin_Button />
-      </Link>
-      <Link to="/kakao">
-        <KakaoLogin_Button />
-      </Link>
-      <GoogleLogin_Button />
+    
+    <div className="loginPage">
+      <NormalLogin />
+      <div className="socialLogin">
+        <div className = "naverLogin">
+          <Link to="/naver">
+          <NaverLogin_Button />
+          </Link>
+        </div>
+        <div className = "kakaoLogin">
+          <Link to="/kakao">
+            <KakaoLogin_Button />
+          </Link> 
+        </div>
+        <div>
+          <Link to ="/google">
+            <GoogleLogin_Button />
+          </Link>  
+        </div>
+      </div> 
     </div>
+
   );
 }
+
 export default Login_Body;
---
+++ src/component/login/loginbody_style.tsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+const Body_Login = styled.div`
+    .page{
+        margin-left: 10%;
+        margin-right: 10%;
+        width: flex;
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        position: relative;
+    }
+    .titleWrap{
+        margin-top: 50px;
+        font-size: 26px;
+        font-weight: 700;
+    }
+    .contentWrap {
+        margin-top: 26px;
+        flex: 1;
+    }
+    .inputTitle {
+        font-size: 12px;
+        font-weight: 600;
+        color: #262626;
+    }
+    .inputWrap {
+        display: flex;
+        border-radius: 8px;
+        padding: 16px;
+        margin-top: 8px;
+        background-color: white;
+        border: 1px solid #e2e0e0;
+    }
+    .inputWrap:focus-within {
+        border: 1px solid #FF4471;
+    }
+    
+    .input {
+        width: 100%;
+        outline: none;
+        border: none;
+        height: 17px;
+        font-size: 14px;
+        font-weight: 400;
+    }
+    
+    .input::placeholder {
+        color: #dadada;
+    }
+    
+    .bottomButton {
+        width: 100%;
+        height: 48px;
+        border: none;
+        font-weight: bold;
+        border-radius: 64px;
+        background-color: #FF4471;
+        color: white;
+        margin-bottom: 16px;
+        cursor: pointer;
+    }
+    
+
+`;
+
+export { Body_Login };
