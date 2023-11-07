47b09896158d16d6dec1aa7dca837bc260b7a3ac
kyoungmin <lanos5019@ajou.ac.kr>
2023-11-08 01:17:31 +0900

    git: pull 9 to 48

--- src/component/footer/detailFooter.tsx
+++ src/component/footer/detailFooter.tsx
@@ -2,8 +2,8 @@ import Navigation from "./footer_style";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 import Participate from "./detailFooterStyle";
-import React, {useState, useEffect} from "react";
-import Modal from 'react-modal';
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -15,7 +15,7 @@ function DetailFooter({ idx }: propsType) {
   const [selectedOption, setSelectedOption] = useState(0);
   const [popUpOpen, setPopUpOpen] = useState(false);
   const [check, setCheck] = useState(false);
-  const[volunteerOrCustomer, setVolORCus] = useState(0);
+  const [volunteerOrCustomer, setVolORCus] = useState(0);
   const navigate = useNavigate();
 
   const onSubmit = async () => {
@@ -27,14 +27,14 @@ function DetailFooter({ idx }: propsType) {
         Authorization: localStorage.getItem("jwt-token"),
       },
       data: {
-        type: selectedOption
-      }
-    }
+        type: selectedOption,
+      },
+    };
     let res = await axios(config);
     console.log(res.data);
     setCheck(false);
     setVolORCus(selectedOption);
-  }
+  };
 
   const cancelVol = async () => {
     setPopUpOpen(false);
@@ -47,64 +47,147 @@ function DetailFooter({ idx }: propsType) {
       },
       data: {
         type: volunteerOrCustomer,
-      }
-    }
+      },
+    };
     let res = await axios(config);
     console.log(res.data);
-  }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get(`https://api-dev.weconnect.support/volunteer/${idx}`, {
-        headers: {
-          Authorization: localStorage.getItem("jwt-token"),
-        },
-      });
+      const { data } = await axios.get(
+        `https://api-dev.weconnect.support/volunteer/${idx}`,
+        {
+          headers: {
+            Authorization: localStorage.getItem("jwt-token"),
+          },
+        }
+      );
 
-      console.log(data.data.volunteer); 
-      if(data.data.volunteer.joined===1||data.data.volunteer.joined===2){
+      console.log(data.data.volunteer);
+      if (
+        data.data.volunteer.joined === 1 ||
+        data.data.volunteer.joined === 2
+      ) {
         setCheck(false);
         setVolORCus(data.data.volunteer.joined);
-      } 
-      else{setCheck(true)}
+      } else {
+        setCheck(true);
+      }
     };
 
     fetchData();
   }, [idx]);
-  
+
   return (
     <Participate>
-      <button style={{width:"100vw", height:"200%", backgroundColor: check ? "#ff4471":"#e2e2e2", border:"none"}} onClick={!check ? () => cancelVol() : () => setPopUpOpen(true)}>{check==true ? "신청하기" : "취소하기"}</button>
+      <button
+        style={{
+          width: "100vw",
+          height: "200%",
+          backgroundColor: check ? "#ff4471" : "#e2e2e2",
+          border: "none",
+        }}
+        onClick={!check ? () => cancelVol() : () => setPopUpOpen(true)}
+      >
+        {check == true ? "신청하기" : "취소하기"}
+      </button>
       <Modal
         className="Popup"
-        isOpen = {popUpOpen}
+        isOpen={popUpOpen}
         onRequestClose={() => setPopUpOpen(false)}
         style={{
           content: {
-            position: 'absolute',
-            bottom: '0',
-            right: '0',
-            left:'0',
-          }
+            position: "absolute",
+            bottom: "0",
+            right: "0",
+            left: "0",
+          },
         }}
-      >  
-        <div style={{backgroundColor:"#e2e2e2",flexDirection:"column", alignItems: "center", display: "flex"}}>
-          <div style={{margin:"1rem 0 0 0",padding: "1rem 0 1rem 0",height:"20vh", width:"100%", backgroundColor:"white",borderRadius:"2rem", display:"flex", alignItems:"center", flexDirection:"column"}}>
-            <button onClick={()=>{setSelectedOption(1)}} style={{border:"none",margin:"0 auto", width:"7rem", height:"7rem", borderRadius: "50%", backgroundColor:`${selectedOption==1? "#ff4471":""}`}}></button>
-            <div style={{fontWeight:"bold"}}>자원 봉사자</div>
+      >
+        <div
+          style={{
+            backgroundColor: "#e2e2e2",
+            flexDirection: "column",
+            alignItems: "center",
+            display: "flex",
+          }}
+        >
+          <div
+            style={{
+              margin: "1rem 0 0 0",
+              padding: "1rem 0 1rem 0",
+              height: "20vh",
+              width: "100%",
+              backgroundColor: "white",
+              borderRadius: "2rem",
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <button
+              onClick={() => {
+                setSelectedOption(1);
+              }}
+              style={{
+                border: "none",
+                margin: "0 auto",
+                width: "7rem",
+                height: "7rem",
+                borderRadius: "50%",
+                backgroundColor: `${selectedOption == 1 ? "#ff4471" : ""}`,
+              }}
+            ></button>
+            <div style={{ fontWeight: "bold" }}>자원 봉사자</div>
           </div>
-          <div style={{margin:"1rem 0 1rem 0",padding: "1rem 0 1rem 0",height:"20vh", width:"100%",backgroundColor:"white", borderRadius:"2rem", display:"flex", alignItems:"center", flexDirection:"column"}}>
-            <button onClick={()=>{setSelectedOption(2)}} style={{border:"none",margin:"0 auto", width:"7rem", height:"7rem", borderRadius: "50%", backgroundColor:`${selectedOption==2? "#ff4471":""}`}}></button>
-            <div style={{fontWeight:"bold"}}>봉사 수요자</div>
+          <div
+            style={{
+              margin: "1rem 0 1rem 0",
+              padding: "1rem 0 1rem 0",
+              height: "20vh",
+              width: "100%",
+              backgroundColor: "white",
+              borderRadius: "2rem",
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <button
+              onClick={() => {
+                setSelectedOption(2);
+              }}
+              style={{
+                border: "none",
+                margin: "0 auto",
+                width: "7rem",
+                height: "7rem",
+                borderRadius: "50%",
+                backgroundColor: `${selectedOption == 2 ? "#ff4471" : ""}`,
+              }}
+            ></button>
+            <div style={{ fontWeight: "bold" }}>봉사 수요자</div>
           </div>
         </div>
-        <div style={{display: "flex", justifyContent: "center", height:"4rem"}}>
-          <button onClick={onSubmit} style={{width: "100%", height: "100%", backgroundColor:"#ff4471", border:"none"}}>신청하기</button>
+        <div
+          style={{ display: "flex", justifyContent: "center", height: "4rem" }}
+        >
+          <button
+            onClick={onSubmit}
+            style={{
+              width: "100%",
+              height: "100%",
+              backgroundColor: "#ff4471",
+              border: "none",
+            }}
+          >
+            신청하기
+          </button>
         </div>
       </Modal>
     </Participate>
   );
 }
-
 
 export default DetailFooter;
