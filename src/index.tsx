38f1bf31dee345d6efb82957b0cb21623c533dd2
kyoungmin <lanos5019@ajou.ac.kr>
2023-11-22 02:31:04 +0900

    test: font apply test

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import GlobalStyle from "./global";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
+    <GlobalStyle />
     <App />
   </BrowserRouter>
 );
